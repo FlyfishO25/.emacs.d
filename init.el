fbe72d9b7605d49b99776cb4b725a522f3e0953d
Markzhou <flyfish7911@163.com>
2021-12-14 21:18:41 +0800

    feat: add magit and diff-hl as git feature
    Added magit, magit-gerrit and diff-hl as git feature, use diff-hl instead of git-gutter

--- init.el
+++ init.el
@@ -193,8 +193,7 @@
 (require 'init-org)
 (add-hook 'org-mode-hook (lambda () (require 'init-roam)))
 (require 'reading-nov)
-
-
+(require 'git-package)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -208,7 +207,7 @@
  '(ns-command-modifier nil)
  '(package-native-compile t)
  '(package-selected-packages
-   '(org-tree-slide org-preview-html org-fragtog org-timeline org-mime toc-org org-rich-yank ob-mermaid ob-rust ob-go org-fancy-priorities org-superstar ox-gfm gameoflife wanderlust anzu benchmark-init org-roam-ui org-roam cnfonts xah-fly-keys spaceline spaceline-all-the-icons spacemacs-theme treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile hlinum god-mode exec-path-from-shell org-pomodoro good-scroll paradox gnu-elpa-keyring-update ivy-prescient zzz-to-char yasnippet-snippets yapfify yaml-mode xclip writegood-mode window-numbering which-key wgrep web-mode vlf use-package string-inflection sourcerer-theme realgud rainbow-delimiters pretty-hydra powerline phi-autopair page-break-lines origami neotree multiple-cursors monokai-theme modern-cpp-font-lock melancholy-theme magit-gerrit lsp-ui lsp-ivy linum-relative json-mode hungry-delete google-c-style git-gutter flyspell-correct-ivy flycheck-ycmd flycheck-pyflakes flycheck-posframe flycheck-popup-tip flex-compile flex-autopair evil elpy ein eglot edit-server doom-themes doom-modeline diminish dashboard dap-mode cuda-mode counsel-etags company-ycmd company-statistics company-quickhelp-terminal company-prescient company-jedi company-box cmake-font-lock clang-format beacon auto-package-update auctex async all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer all-the-icons-gnus all-the-icons-dired all-the-icons-completion ace-flyspell 2048-game)))
+   '(diff-hl org-tree-slide org-preview-html org-fragtog org-timeline org-mime toc-org org-rich-yank ob-mermaid ob-rust ob-go org-fancy-priorities org-superstar ox-gfm gameoflife wanderlust anzu benchmark-init org-roam-ui org-roam cnfonts xah-fly-keys spaceline spaceline-all-the-icons spacemacs-theme treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile hlinum god-mode exec-path-from-shell org-pomodoro good-scroll paradox gnu-elpa-keyring-update ivy-prescient zzz-to-char yasnippet-snippets yapfify yaml-mode xclip writegood-mode window-numbering which-key wgrep web-mode vlf use-package string-inflection sourcerer-theme realgud rainbow-delimiters pretty-hydra powerline phi-autopair page-break-lines origami neotree multiple-cursors monokai-theme modern-cpp-font-lock melancholy-theme magit-gerrit lsp-ui lsp-ivy linum-relative json-mode hungry-delete google-c-style git-gutter flyspell-correct-ivy flycheck-ycmd flycheck-pyflakes flycheck-posframe flycheck-popup-tip flex-compile flex-autopair evil elpy ein eglot edit-server doom-themes doom-modeline diminish dashboard dap-mode cuda-mode counsel-etags company-ycmd company-statistics company-quickhelp-terminal company-prescient company-jedi company-box cmake-font-lock clang-format beacon auto-package-update auctex async all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer all-the-icons-gnus all-the-icons-dired all-the-icons-completion ace-flyspell 2048-game)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
