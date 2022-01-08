a6d0021bde798d72b4cbd15a94d532bffd77c710
FlyfishO25 <zhouansheng0125@163.com>
2022-01-08 14:49:47 +0800

    feat: Add flycheck plugin and rename file
    Add: flycheck. Rename: reading-nov.el => reading.el

---
+++ lisp/reading.el
@@ -0,0 +1,69 @@
+;;; init.el --- Initialize reding envoirment file.	-*- lexical-binding: t -*-
+
+;; Copyright (C) 2022 FlyfishO25
+
+;; Author: FlyfishO25 <markzhou0125@gmail.com>
+;; URL: https://github.com/FlyfishO25/.emacs.d
+
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+
+;;; Commentary:
+;; Reading enviorment configures
+
+;;; Code:
+
+(use-package nov
+  :mode ("\\.epub\\'" . nov-mode)
+  :hook (nov-mode . my-nov-setup)
+  :init
+  (defun my-nov-setup ()
+    "Setup `nov-mode' for better reading experience."
+    (visual-line-mode 1))
+    ;; (face-remap-add-relative 'variable-pitch :family "Times New Roman" :height 1.5))
+  :config
+  (with-no-warnings
+    ;; HACK: errors while opening `nov' files with Unicode characters
+    ;; @see https://github.com/wasamasa/nov.el/issues/63
+    (defun my-nov-content-unique-identifier (content)
+      "Return the the unique identifier for CONTENT."
+      (let* ((name (nov-content-unique-identifier-name content))
+             (selector (format "package>metadata>identifier[id='%s']"
+                               (regexp-quote name)))
+             (id (car (esxml-node-children (esxml-query selector content)))))
+        (and id (intern id))))
+    (advice-add #'nov-content-unique-identifier :override #'my-nov-content-unique-identifier))
+
+  ;; Fix encoding issue on Windows
+  (when (eq system-type 'windows-nt)
+    (setq process-coding-system-alist
+          (cons `(,nov-unzip-program . (gbk . gbk))
+                process-coding-system-alist))))
+
+(use-package calibredb
+  :defer t
+  :config
+  (setq calibredb-root-dir "~/Documents/Calibre"
+        calibredb-db-dir (expand-file-name "metadata.db" calibredb-root-dir)
+        calibredb-library-alist '("~/Documents/Calibre/")
+        calibredb-program "/Applications/calibre.app/Contents/MacOS/calibredb")
+  )
+
+(provide 'reading)
+
+;;; reading.el ends here
