0e21375ed13dbf95ca5a0fc67c5ed3d1c5ddfdee
FlyfishO25 <zhouansheng0125@163.com>
2022-02-27 14:55:33 +0800

    improvement: Set default position of lsp-ui-doc to 'at-point
    Set lsp-ui-doc-position to 'at-point due to it should be a better default

--- lisp/complete-lsp.el
+++ lisp/complete-lsp.el
@@ -227,6 +227,7 @@
        :init (setq lsp-ui-sideline-show-diagnostics nil
                    lsp-ui-sideline-ignore-duplicate t
                    lsp-ui-doc-delay 0.1
+                   lsp-ui-doc-position 'at-point
                    lsp-ui-doc-border (face-foreground 'font-lock-comment-face nil t)
                    lsp-ui-imenu-colors `(,(face-foreground 'font-lock-keyword-face)
                                          ,(face-foreground 'font-lock-string-face)
