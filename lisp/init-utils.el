7b6455a5b6403d1e24d47576cf6a0416e8b7909a
FlyfishO25 <zhouansheng0125@163.com>
2022-03-02 20:04:00 +0800

    feat: Add some useful tools (belong to previous commit)

---
+++ lisp/init-utils.el
@@ -0,0 +1,14 @@
+;;; init-utils.el --- init useful tools  -*- lexical-binding: t; -*-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Enable terminal emacs to copy and paste from system clipboard
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun osx-copy (beg end)
+  (interactive "r")
+  (call-process-region beg end  "pbcopy"))
+
+(defun osx-paste ()
+  (interactive)
+  (if (region-active-p) (delete-region (region-beginning) (region-end)) nil)
+  (call-process "pbpaste" nil t nil))
+
